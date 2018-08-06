c306d5a7f35373c89bcdceb92066910b934f21c2
SiHun Sung <sungsihun@gmail.com>
2018-08-06 17:44:23 -0400

    Initial Commit

---
+++ 2PlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2PlayerMath
+//
+//  Created by NICE on 2018-08-06.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
